75c6dba4997e496e6457ebf3bb36c5a5822add08
Emanuele Paolini <emanuele.paolini@gmail.com>
2021-08-17 10:39:49 +0200

    better (?) connection handling

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var connection_count = 0;
 function print(player, msg) {
 //    console.log(`print(${player.id}, "${msg}")`);
     connections.forEach(connection => {
-        if (connection.player && connection.player.id === player.id) {
+        if (connection.player && connection.ws && connection.player.id === player.id) {
             console.log(`${connection.id} OUT ${msg}`);
             connection.ws.send("OUT " + msg);
         }
@@ -30,7 +30,7 @@ function print(player, msg) {
 
 function broadcast(item, msg) {
     connections.forEach(connection => {
-        if (connection.player && connection.player.id !== item.id) {
+        if (connection.player && connection.ws && connection.player.id !== item.id) {
             console.log(`${connection.id} OUT ${msg}`);
             connection.ws.send("OUT " + msg);
         }
@@ -43,9 +43,6 @@ dungeon.world.broadcast = broadcast;
 wss.on('connection', (ws, req, client) => {
     connection_count ++;
     const connection_id = connection_count;
-    //send immediatly a feedback to the incoming connection    
-    ws.send(`CON ${connection_id}`);
-    console.log(`${connection_id} CONNECTED`);
 
     const connection = {
         "id": connection_id,
@@ -55,15 +52,17 @@ wss.on('connection', (ws, req, client) => {
         "world": null
     };
 
-    var player = null;
-
     connections.push(connection);
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send(`CON ${connection_id}`);
+    console.log(`${connection_id} CONNECTED`);
 
     ws.on('message', (message) => {
 //        console.log("--- " + message);
         if (message.startsWith("STA ")) {
             message = message.substring(4);
-            player = dungeon.world.add_item({
+            var player = dungeon.world.add_item({
                 attributes: ["player", "container"],
                 name: message,
                 description: "un bel tipo",
@@ -77,10 +76,14 @@ wss.on('connection', (ws, req, client) => {
             dungeon.world.where(player);
         } else if (message.startsWith("CMD ")) {
             message = message.substring(4);
-            dungeon.play(player, dungeon.world, message);
+            dungeon.play(connection.player, connection.world, message);
         } else {
             ws.send(`Hello, you wrote -> ${message}`);
         }
+    });
+    ws.on('close', (code, msg) => {
+        console.log(`${connection_id} CLOSE ${code} ${msg}`);
+        connection.ws = null;
     });
 });
 
